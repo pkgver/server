554aa116a51b1d3b6d861908605fc13ac81f0eef
dsymbol <dsymbol@users.noreply.github.com>
2023-03-06 01:56:35 +0200

    nix-shell -> nix develop

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "pkgver server development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            (python310.withPackages (ps: with ps; [
+              fastapi
+              uvicorn
+              GitPython
+            ]))
+          ];
+        };
+      }
+    );
+}
